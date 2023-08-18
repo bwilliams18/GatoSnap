7b119716aabc66a8df0f28cba2b48198999389bd
Braden Williams <braden@bradenwill.com>
2023-08-18 00:02:30 -0500

    tweaked deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 cd frontend
+yarn install
 yarn build
 cd ..
 rm -r backend/static/*
 mkdir -p backend/static
 cp -r frontend/build/* backend/static/
 cd backend
+poetry install --only main
 poetry run alembic upgrade head
